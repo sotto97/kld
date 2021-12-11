3031e66bc1dae05a8321ba40f1bccf723891a337
sotto97 <sotto.bw97@gmail.com>
2021-12-11 23:39:58 +0900

    ログイン後の画面の遷移を変更

--- server/kld/resources/views/category/index.blade.php
+++ server/kld/resources/views/category/index.blade.php
@@ -1,23 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+<div class="container w-full">
+    <div class="">
+        <a href="{{ route('category.create') }}" class="">
+            <p class="w-1/2 text-center py-2 mx-auto bg-gray-700 text-white rounded-full">カテゴリを登録する</p>
+        </a>
     </div>
+    <div class="">
+        <a href="{{ route('article.create') }}" class="">
+            <p class="w-1/2 text-center py-2 mx-auto bg-gray-700 text-white rounded-full">今日のナレッジを登録する</p>
+        </a>
+    </div>
+    <table class="table table-hover table-dark text-white">
+        <thead class="w-full">
+            <tr>
+                <th class="w-4/12">ID</th>
+                <th class="w-4/12">カテゴリ名</th>
+                <th class="w-4/12"></th>
+            </tr>
+        </thead>
+        <tbody class="text-left">
+            <tr>
+                <td>1</td>
+                <td>デモクリニック</td>
+                <td>
+                    <p class="transition mb-0 duration-500 w-full bg-green-500 text-center rounded-full">対応中</p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 @endsection
