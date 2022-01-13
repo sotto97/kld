72fdb36fb5db72abf0e8bb7daff06cf96f0d52d1
sotto97 <sotto.bw97@gmail.com>
2022-01-14 07:53:29 +0900

    [カテゴリ]indexのレイアウト変更

--- server/kld/resources/views/category/index.blade.php
+++ server/kld/resources/views/category/index.blade.php
@@ -1,33 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container w-full">
+<section id="content" class="container w-full">
     <div class="">
         <a href="{{ route('category.create') }}" class="">
-            <p class="w-1/2 text-center py-2 mx-auto bg-gray-700 text-white rounded-full">カテゴリを登録する</p>
+            <p class="w-1/2 text-center py-2 mx-auto bg-gray-700 text-white rounded-full">Add New Category</p>
         </a>
     </div>
-    <div class="">
-        <a href="{{ route('article.create') }}" class="">
-            <p class="w-1/2 text-center py-2 mx-auto bg-gray-700 text-white rounded-full">今日のナレッジを登録する</p>
-        </a>
-    </div>
-    <table class="table table-hover table-dark text-white">
-        <thead class="w-full">
-            <tr>
-                <th class="w-4/12">ID</th>
-                <th class="w-4/12">カテゴリ名</th>
-                <th class="w-4/12"></th>
-            </tr>
-        </thead>
-        <tbody class="text-left">
-            @foreach ($categories as $category)
-            <tr>
-                <td>{{ $category->id }}</td>
-                <td>{{ $category->name }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+    <section id="categories" class="pt-2">
+        <table class="table table-hover table-dark text-white">
+            <thead class="w-full">
+                <tr>
+                    <th class="w-4/12">ID</th>
+                    <th class="w-4/12">Name</th>
+                    <th class="w-4/12"></th>
+                </tr>
+            </thead>
+            <tbody class="text-left">
+                @foreach ($categories as $category)
+                <tr class="">
+                    <td>{{ $category->id }}</td>
+                    <td>{{ $category->name }}</td>
+                    <td class="text-center"><button class="bg-teal-500 hover:bg-teal-600 text-white py-1 w-1/2 mx-auto rounded-full">edit</button></td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </section>
+</section>
 @endsection
