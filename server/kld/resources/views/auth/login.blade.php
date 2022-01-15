8625d863db7115b2885403cb88cdb5ca18a48314
sotto97 <sotto.bw97@gmail.com>
2022-01-15 21:26:43 +0900

    [Auth]不要な文言を削除

--- server/kld/resources/views/auth/login.blade.php
+++ server/kld/resources/views/auth/login.blade.php
@@ -8,12 +8,6 @@
                 <div class="card-header">{{ __('ログイン') }}</div>
 
                 <div class="card-body">
-                    <div class="max-w-sm mx-auto flex p-5 m-10 bg-white rounded-3g shadow-xl">
-                        <div class="ml-6 pt-1">
-                            <h4 class="text-xl text-green-600 leading-tight">Tailwind</h4>
-                            <p class="text-base text-grey-600 leading-normal">check the display!</p>
-                        </div>
-                    </div>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
