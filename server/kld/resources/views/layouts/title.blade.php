e6da5560b90ae4c23dd69df61b62d6c2f71e8ed2
sotto97 <sotto.bw97@gmail.com>
2022-02-08 23:53:03 +0900

    [全体]ヘッダのタイトルの文字を追加

--- server/kld/resources/views/layouts/title.blade.php
+++ server/kld/resources/views/layouts/title.blade.php
@@ -1,14 +1,27 @@
+{{-- 認証系 --}}
+@if (Route::is('login'))
+<p>ログイン</p>
+@elseif (Route::is('register'))
+<p>ユーザー新規登録</p>
 {{-- ナレッジ系 --}}
-@if (Route::is('article.index'))
+@elseif (Route::is('article.index'))
 <p>ナレッジ一覧</p>
 @elseif (Route::is('article.create'))
 <p>ナレッジ作成</p>
 @elseif (Route::is('article.edit'))
 <p>ナレッジ編集</p>
-
 {{-- カテゴリ系 --}}
 @elseif (Route::is('category.index'))
 <p>カテゴリ一覧</p>
 @elseif (Route::is('category.create'))
 <p>カテゴリ作成</p>
+@elseif (Route::is('category.edit'))
+<p>カテゴリ編集</p>
+{{-- クライアント系 --}}
+@elseif (Route::is('client.index'))
+<p>クライアント一覧</p>
+@elseif (Route::is('client.create'))
+<p>クライアント作成</p>
+@elseif (Route::is('client.edit'))
+<p>クライアント編集</p>
 @endif
